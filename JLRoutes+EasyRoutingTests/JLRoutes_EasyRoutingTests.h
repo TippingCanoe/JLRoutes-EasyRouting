837acc99dc22254f0eb4af790d575fa2aa2eddc9
Jeff Friesen <j.friesen@tippingcanoe.com>
2013-05-28 15:47:08 -0500

    Initial Commit

---
+++ JLRoutes+EasyRoutingTests/JLRoutes_EasyRoutingTests.h
@@ -0,0 +1,13 @@
+//
+//  JLRoutes_EasyRoutingTests.h
+//  JLRoutes+EasyRoutingTests
+//
+//  Created by Jeff on 2013-05-28.
+//  Copyright (c) 2013 Jeff Friesen. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface JLRoutes_EasyRoutingTests : SenTestCase
+
+@end
---
+++ JLRoutes+EasyRoutingTests/JLRoutes_EasyRoutingTests.m
@@ -0,0 +1,32 @@
+//
+//  JLRoutes_EasyRoutingTests.m
+//  JLRoutes+EasyRoutingTests
+//
+//  Created by Jeff on 2013-05-28.
+//  Copyright (c) 2013 Jeff Friesen. All rights reserved.
+//
+
+#import "JLRoutes_EasyRoutingTests.h"
+
+@implementation JLRoutes_EasyRoutingTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in JLRoutes+EasyRoutingTests");
+}
+
+@end
